962904d338b10054edd3088aa096199f48a135d8
Will Diederich <wcd4541@gmail.com>
2026-05-17 12:29:31 -0400

    feat: add weeks route

---
+++ routes/weeks.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    // just some sample data
+    const score = {
+        player_name: "Will Diederich",
+        quota: 9,
+        scores: [5,5,5,4,3,7,6,4,9],
+        total_points: 9
+    }
+
+  res.render('weeks', { score });
+});
+
+module.exports = router;
